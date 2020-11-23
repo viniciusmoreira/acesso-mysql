03202c25eed2733c6106f6e48ad7c0932696405c
Vinicius Moreira <vinicius.moreira@godoo.com.br>
2020-11-23 15:15:53 -0300

    Ajuste para uso do DOTENV

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2/promise');
 module.exports = mysql.createPool({
   // Comando utilizado para descobrir endereço do container.
   // docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
-  host: '172.17.0.2', 
-  port: 3306,
+  host: process.env.DATABASE_HOST, 
+  port: process.env.DATABASE_PORT,
   user:process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
-  database: 'cat-products',
+  database: process.env.DATABASE_NAME,
   waitForConnections: true,
   connectionLimit:20
 });
--- poc/test-products-update.js
+++ poc/test-products-update.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
 const mysql = require('mysql2/promise');
 
-const run = async (id, values) => {
+const run = async (values) => {
   const connection = await mysql.createConnection({
     // Comando utilizado para descobrir endereço do container.
     // docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
-    host: '172.17.0.2', 
-    port: 3306,
-    user:'root',
-    password: 'root',
-    database: 'cat-products'
+    host: process.env.DATABASE_HOST, 
+    port: process.env.DATABASE_PORT,
+    user:process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME
   })
 
   try {
